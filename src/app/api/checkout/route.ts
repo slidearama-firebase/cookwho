146f564976884da7c0cbbc9f48dd94a79f227439
slidearama-firebase <slidearama@gmail.com>
2026-01-19 22:25:14 +0000

    Delete src directory

--- src/app/api/checkout/route.ts
+++
@@ -1,6 +0,0 @@
-
-import { NextResponse } from 'next/server';
-
-export async function POST(req: Request) {
-  return new NextResponse('Stripe Checkout is not enabled.', { status: 501 });
-}
--- src/components/checkout-form.tsx
+++
@@ -1,7 +0,0 @@
-
-'use client';
-
-// This component is not used as Stripe payments are currently disabled.
-export default function CheckoutForm() {
-  return null;
-}
